0d1aeb6d12d05e98d23b40bd2f55bf093e96dfaf
AkramGalal <61739568+AkramGalal@users.noreply.github.com>
2025-08-30 17:58:29 +0200

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,56 +1,66 @@
 #!/bin/bash
 
+# ========= Variables =========
 TOMURL="https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz"
-#dnf -y install java-11-openjdk java-11-openjdk-devel
-dnf install -y java-11-amazon-corretto-devel java-11-amazon-corretto
-dnf install git maven wget -y
+JAVA_HOME_DIR="/usr/lib/jvm/java-11-amazon-corretto"
+APP_PROPS="/home/ec2-user/application.properties"
+
+# ========= Install Dependencies =========
+sudo dnf update -y
+sudo dnf install -y java-11-amazon-corretto java-11-amazon-corretto-devel git maven wget rsync tar unzip mysql
+
+# ========= Download & Install Tomcat =========
 cd /tmp/
 wget $TOMURL -O tomcatbin.tar.gz
-EXTOUT=`tar xzvf tomcatbin.tar.gz`
-TOMDIR=`echo $EXTOUT | cut -d '/' -f1`
-useradd tomcat --shell /sbin/nologin -md /usr/local/tomcat
-rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat/
-chown -R tomcat.tomcat /usr/local/tomcat
-rm -rf /etc/systemd/system/tomcat.service
+EXTOUT=$(tar xzvf tomcatbin.tar.gz)
+TOMDIR=$(echo $EXTOUT | cut -d '/' -f1)
 
-cat <<EOT>> /etc/systemd/system/tomcat.service
+# Create tomcat user & install directory
+sudo useradd tomcat --shell /sbin/nologin -md /usr/local/tomcat || true
+sudo rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat/
+sudo chown -R tomcat:tomcat /usr/local/tomcat
+
+# ========= Create Systemd Service =========
+sudo rm -f /etc/systemd/system/tomcat.service
+
+cat <<EOT | sudo tee /etc/systemd/system/tomcat.service
 [Unit]
-Description=Tomcat
+Description=Apache Tomcat 9
 After=network.target
 
 [Service]
+Type=forking
 User=tomcat
 Group=tomcat
 WorkingDirectory=/usr/local/tomcat
-Environment=JAVA_HOME=/usr/lib/jvm/jre
-Environment=CATALINA_PID=/var/tomcat/%i/run/tomcat.pid
+Environment=JAVA_HOME=$JAVA_HOME_DIR
+Environment=CATALINA_PID=/usr/local/tomcat/temp/tomcat.pid
 Environment=CATALINA_HOME=/usr/local/tomcat
-Environment=CATALINE_BASE=/usr/local/tomcat
-ExecStart=/usr/local/tomcat/bin/catalina.sh run
+Environment=CATALINA_BASE=/usr/local/tomcat
+ExecStart=/usr/local/tomcat/bin/startup.sh
 ExecStop=/usr/local/tomcat/bin/shutdown.sh
 RestartSec=10
-Restart=always
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-
 EOT
 
-systemctl daemon-reload
-systemctl start tomcat
-systemctl enable tomcat
+# ========= Enable & Start Tomcat =========
+sudo systemctl daemon-reload
+sudo systemctl enable tomcat
+sudo systemctl start tomcat
 
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
-#cd vprofile-project
-#mvn install
-#systemctl stop tomcat
-#sleep 20
-#rm -rf /usr/local/tomcat/webapps/ROOT*
-#cp target/vprofile-v2.war /usr/local/tomcat/webapps/ROOT.war
-#chown -R tomcat.tomcat /usr/local/tomcat
-#systemctl start tomcat
-#sleep 20
-#systemctl stop firewalld
-#systemctl disable firewalld
-#cp /vagrant/application.properties /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/application.properties
-#systemctl restart tomcat
+# ========= Clone & Build vprofile Project =========
+cd /opt/
+sudo git clone -b main https://github.com/hkhcoder/vprofile-project.git
+cd vprofile-project
+mvn clean install -DskipTests
+
+# ========= Deploy WAR into Tomcat =========
+sudo systemctl stop tomcat
+sleep 10
+sudo rm -rf /usr/local/tomcat/webapps/ROOT*
+sudo cp target/vprofile-v2.war /usr/local/tomcat/webapps/ROOT.war
+sudo chown -R tomcat:tomcat /usr/local/tomcat/webapps
+sudo systemctl start tomcat
